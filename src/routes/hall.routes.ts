264af2d1be54ae0ce574d690f3299d09f4c72c6d
Saltman0 <baudoin.mathieu@protonmail.com>
2025-01-13 22:06:02 +0100

    Create hall.routes.ts

---
+++ src/routes/hall.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import passport from "../middleware/passport";
+import * as hallController from "../controllers/hall.controller";
+
+const router: Router = Router();
+
+router.get("cinema/:cinemaId/hall", passport.authenticate("jwt", { session: false }), hallController.getHalls);
+router.get("/hall/:id", passport.authenticate("jwt", { session: false }), hallController.getHallById);
+router.post("cinema/:cinemaId/hall", passport.authenticate("jwt", { session: false }), hallController.createHall);
+router.put("/hall/:id", passport.authenticate("jwt", { session: false }), hallController.updateHall);
+router.delete("/hall/:id", passport.authenticate("jwt", { session: false }), hallController.deleteHall);
+
+export default router;
