ca110083cffff7049678b85c1a9c71269d10cdbc
Saltman0 <baudoin.mathieu@protonmail.com>
2025-01-13 21:41:31 +0100

    Create cinema.controller.ts

---
+++ src/controllers/cinema.controller.ts
@@ -0,0 +1,91 @@
+import { Request, Response } from "express";
+import * as cinemaRepository from "../repository/cinema.repository";
+
+export async function getCinemas(req: Request, res: Response) {
+    try {
+        const cinemas = await cinemaRepository.findCinemas();
+
+        if (cinemas !== null) {
+            res.status(200).json(cinemas);
+        } else {
+            res.status(404).json({ message : `Cinemas not found.` });
+        }
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+}
+
+export async function getCinemaById(req: Request, res: Response) {
+    try {
+        const cinema = await cinemaRepository.findCinemaById(
+            parseInt(req.params.id)
+        );
+
+        if (cinema !== null) {
+            res.status(200).json(cinema);
+        } else {
+            res.status(404).json({ message : `Cinema ${req.params.id} not found.` });
+        }
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+}
+
+export async function createCinema(req: Request, res: Response) {
+    try {
+        const cinemaToCreate = await cinemaRepository.insertCinema(
+            req.body.name,
+            req.body.address,
+            parseInt(req.body.postalCode),
+            req.body.city,
+            req.body.phoneNumber,
+            req.body.openHour,
+            req.body.closeHour
+        );
+
+        res.status(201).json(cinemaToCreate);
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+}
+
+export async function updateCinema(req: Request, res: Response) {
+    try {
+        const cinemaToUpdate = await cinemaRepository.updateCinema(
+            parseInt(req.params.id),
+            req.body.name,
+            req.body.address,
+            parseInt(req.body.postalCode),
+            req.body.city,
+            req.body.phoneNumber,
+            req.body.openHour,
+            req.body.closeHour
+        );
+
+        res.status(200).json(cinemaToUpdate);
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+}
+
+export async function deleteCinema(req: Request, res: Response) {
+    try {
+        const cinemaToDelete = await cinemaRepository.deleteCinema(
+            parseInt(req.params.id)
+        );
+
+        res.status(200).json({ message: "Cinema deleted successfully." });
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+}
