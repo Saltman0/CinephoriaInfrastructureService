d27e8c9b57740df7940f4e504cdd35cfa8e3110b
Saltman0 <baudoin.mathieu@protonmail.com>
2025-01-14 10:54:04 +0100

    Hall routes fixed

--- src/routes/hall.routes.ts
+++ src/routes/hall.routes.ts
@@ -4,9 +4,9 @@ import * as hallController from "../controllers/hall.controller";
 
 const router: Router = Router();
 
-router.get("cinema/:cinemaId/hall", passport.authenticate("jwt", { session: false }), hallController.getHalls);
+router.get("/cinema/:cinemaId/hall", passport.authenticate("jwt", { session: false }), hallController.getHalls);
 router.get("/hall/:id", passport.authenticate("jwt", { session: false }), hallController.getHallById);
-router.post("cinema/:cinemaId/hall", passport.authenticate("jwt", { session: false }), hallController.createHall);
+router.post("/cinema/:cinemaId/hall", passport.authenticate("jwt", { session: false }), hallController.createHall);
 router.put("/hall/:id", passport.authenticate("jwt", { session: false }), hallController.updateHall);
 router.delete("/hall/:id", passport.authenticate("jwt", { session: false }), hallController.deleteHall);
 
