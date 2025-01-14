8df6234de88e578adebf49efd30ff88ea5b7b1df
Saltman0 <baudoin.mathieu@protonmail.com>
2025-01-14 22:32:43 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { app, logger, port } from "./app";
 
 app.listen(port, () => {
-  logger.info(`Booking service : Server is running on port ${port} in ${process.env.NODE_ENV} mode`);
+  logger.info(`Infrastructure service : Server is running on port ${port} in ${process.env.NODE_ENV} mode`);
 });
