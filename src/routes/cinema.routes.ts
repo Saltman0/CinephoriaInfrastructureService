12140ba9ca049583f6b5e98c7b1e2eef31e76a04
Saltman0 <baudoin.mathieu@protonmail.com>
2025-09-06 15:32:13 +0200

    🎨 Cinema routes updated

--- src/routes/cinema.routes.ts
+++ src/routes/cinema.routes.ts
@@ -4,8 +4,8 @@ import * as cinemaController from "../controllers/cinema.controller";
 
 const router: Router = Router();
 
-router.get("/cinema", passport.authenticate("jwt", { session: false }), cinemaController.getCinemas);
-router.get("/cinema/:cinemaId", passport.authenticate("jwt", { session: false }), cinemaController.getCinemaById);
+router.get("/cinema", cinemaController.getCinemas);
+router.get("/cinema/:cinemaId", cinemaController.getCinemaById);
 router.post("/cinema", passport.authenticate("jwt", { session: false }), cinemaController.createCinema);
 router.put("/cinema/:cinemaId", passport.authenticate("jwt", { session: false }), cinemaController.updateCinema);
 router.delete("/cinema/:cinemaId", passport.authenticate("jwt", { session: false }), cinemaController.deleteCinema);
