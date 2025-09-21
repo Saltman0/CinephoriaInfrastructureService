5c422f14f07fedafd8cf61c3a58b0aa86cbeaa6a
Saltman0 <baudoin.mathieu@protonmail.com>
2025-09-21 17:20:48 +0200

    ✨ ResetDatabase SQL added

---
+++ src/sql/resetDatabase.sql
@@ -0,0 +1,4 @@
+-- Cinéphoria Infrastructure service database
+DROP DATABASE IF EXISTS "cinephoriaInfrastructureServiceDatabase";
+CREATE DATABASE "cinephoriaInfrastructureServiceDatabase";
+-- Cinéphoria Infrastructure service database
