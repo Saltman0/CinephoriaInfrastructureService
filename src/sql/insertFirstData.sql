c53593f6e36aa3e77394cc88013e8d93dd15ce61
Saltman0 <baudoin.mathieu@protonmail.com>
2025-09-21 18:21:18 +0200

    ✨ InsetFirstData SQL updated

--- src/sql/insertFirstData.sql
+++ src/sql/insertFirstData.sql
@@ -1,76 +1,104 @@
 -- Start the SQL transaction
 BEGIN;
 
--- Insert cinema
-INSERT INTO "cinema" ("name", "address", "postalCode", "city", "phoneNumber", "openHour", "closeHour")
-VALUES ('Cinéphoria Nantes', '25, avenue du Breton', 44000, 'Nantes', '06 45 89 53 21', '08:00:00', '23:30:00');
--- Insert cinema
+    -- Cinemas
+    INSERT INTO "cinema" ("name", "address", "postalCode", "city", "phoneNumber", "openHour", "closeHour")
+    VALUES ('Cinéphoria Nantes', '25, avenue du Breton', 44000, 'Nantes', '06 45 89 53 21', '08:00:00', '23:30:00')
+    ;
 
-SAVEPOINT cinema_savepoint;
+    -- Halls
+    INSERT INTO "hall" ("number", "projectionQuality", "cinemaId")
+    VALUES (1, 'IMAX', 1),
+           (2, '4DX', 1),
+           (1, 'Standard', 1),
+           (2, 'IMAX', 1),
+           (1, 'Dolby Atmos', 1),
+           (2, 'Standard', 1)
+    ;
 
--- Insert hall
-INSERT INTO "hall" ("number", "projectionQuality", "cinemaId")
-VALUES (1, 'HD', 1),
-       (2, '4K', 1),
-       (3, 'SD', 1),
-       (4, 'HD', 1),
-       (5, 'HD', 1),
-       (6, '4K', 1);
--- Insert hall
+    -- Seats
+    INSERT INTO "seat" ("row", "number", "hallId")
+    VALUES
+    -- Hall 1
+    ('A', 1, 1),
+    ('A', 2, 1),
+    ('B', 1, 1),
+    ('B', 2, 1),
+    ('C', 1, 1),
+    ('C', 2, 1),
+    ('D', 1, 1),
+    ('D', 2, 1),
+    ('E', 1, 1),
+    ('E', 2, 1),
+    -- Hall 2
+    ('A', 1, 2),
+    ('A', 2, 2),
+    ('B', 1, 2),
+    ('B', 2, 2),
+    ('C', 1, 2),
+    ('C', 2, 2),
+    ('D', 1, 2),
+    ('D', 2, 2),
+    ('E', 1, 2),
+    ('E', 2, 2),
+    -- Hall 3
+    ('A', 1, 3),
+    ('A', 2, 3),
+    ('B', 1, 3),
+    ('B', 2, 3),
+    ('C', 1, 3),
+    ('C', 2, 3),
+    ('D', 1, 3),
+    ('D', 2, 3),
+    ('E', 1, 3),
+    ('E', 2, 3),
+    -- Hall 4
+    ('A', 1, 4),
+    ('A', 2, 4),
+    ('B', 1, 4),
+    ('B', 2, 4),
+    ('C', 1, 4),
+    ('C', 2, 4),
+    ('D', 1, 4),
+    ('D', 2, 4),
+    ('E', 1, 4),
+    ('E', 2, 4),
+    -- Hall 5
+    ('A', 1, 5),
+    ('A', 2, 5),
+    ('B', 1, 5),
+    ('B', 2, 5),
+    ('C', 1, 5),
+    ('C', 2, 5),
+    ('D', 1, 5),
+    ('D', 2, 5),
+    ('E', 1, 5),
+    ('E', 2, 5),
+    -- Hall 6
+    ('A', 1, 6),
+    ('A', 2, 6),
+    ('B', 1, 6),
+    ('B', 2, 6),
+    ('C', 1, 6),
+    ('C', 2, 6),
+    ('D', 1, 6),
+    ('D', 2, 6),
+    ('E', 1, 6),
+    ('E', 2, 6)
+    ;
 
-SAVEPOINT hall_savepoint;
-
--- Insert seat
-INSERT INTO "seat" ("row", "number", "hallId")
-VALUES ('A', 1, 1),
-       ('A', 2, 1),
-       ('B', 1, 1),
-       ('B', 2, 1),
-       ('C', 1, 1),
-       ('C', 2, 1),
-       ('A', 1, 2),
-       ('A', 2, 2),
-       ('B', 1, 2),
-       ('B', 2, 2),
-       ('C', 1, 2),
-       ('C', 2, 2),
-       ('A', 1, 3),
-       ('A', 2, 3),
-       ('B', 1, 3),
-       ('B', 2, 3),
-       ('C', 1, 3),
-       ('C', 2, 3),
-       ('A', 1, 4),
-       ('A', 2, 4),
-       ('B', 1, 4),
-       ('B', 2, 4),
-       ('C', 1, 4),
-       ('C', 2, 4),
-       ('A', 1, 5),
-       ('A', 2, 5),
-       ('B', 1, 5),
-       ('B', 2, 5),
-       ('C', 1, 5),
-       ('C', 2, 5),
-       ('A', 1, 6),
-       ('A', 2, 6),
-       ('B', 1, 6),
-       ('B', 2, 6),
-       ('C', 1, 6),
-       ('C', 2, 6);
--- Insert seat
-
-SAVEPOINT seat_savepoint;
-
--- Insert incident
-INSERT INTO "incident" ("type", "description", "date", "solved", "hallId")
-VALUES ('Endommagement', 'Quelque chose est cassé dans cette salle.', '2025-05-04 12:15:00', false, 1),
-       ('Technique', 'Le film ne démarre pas.', '2025-05-02 14:30:00', true, 2),
-       ('Propreté', 'La rangée 1 est sale.', '2025-05-03 10:10:00', true, 4),
-       ('Humain', 'Une bagarre a éclaté dans la salle.', '2025-05-04 16:20:00', false, 5);
--- Insert incident
-
-SAVEPOINT incident_savepoint;
+    -- Incidents
+    INSERT INTO "incident" ("type", "description", "date", "solved", "hallId")
+    VALUES ('Technique', 'Problème de projection du film', '2025-09-20 18:30:00', false, 1),
+           ('Nettoyage', 'Sièges sales au rang A', '2025-09-19 21:00:00', false, 2),
+           ('Sécurité', 'Spectateur perturbant', '2025-09-18 20:00:00', true, 3),
+           ('Maintenance', 'Siège A2 cassé', '2025-09-17 19:00:00', false, 4),
+           ('Incendie', 'Feu déclaré au rang B', '2025-09-15 10:00:00', true, 5),
+           ('Technique', 'Problème de son dans la salle', '2025-09-14 18:00:00', false, 6)
+    ;
 
 -- Commit if successful
 COMMIT;
+
+-- If something fails instead
+ROLLBACK;
